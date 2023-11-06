dba554558a3bc71e6ede6d583f009f99b051a087
Dillion Megida <dillionmegida@gmail.com>
2023-11-06 09:12:40 +0100

    setup gtag and change logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -32,11 +23,24 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-0M3YHJDMSD"],
+        gtagConfig: {
+          head: true,
+          anonymize_ip: true,
+          cookie_expires: 0,
+          send_page_view: true,
+        },
+        pluginConfig: {
+          head: false,
+          delayOnRouteUpdate: 0,
+        },
       },
     },
   ],
