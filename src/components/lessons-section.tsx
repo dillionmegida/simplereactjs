c7f9d72ac5370cfda81dbd824de8847ae4d36298
Dillion Megida <dillionmegida@gmail.com>
2023-11-13 10:26:37 +0100

    improve homepage style

--- src/components/lessons-section.tsx
+++ src/components/lessons-section.tsx
@@ -4,34 +4,127 @@ import NewTabLink from "../components/new-tab-link"
 
 const Section = styled.section`
   background-color: color-mix(in srgb, black, transparent 90%);
-  margin-top: 60px;
+  margin-top: 80px;
+
+  .heading {
+    position: relative;
+    top: -30px;
+    padding: 20px;
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+    /* border: 4px solid color-mix(in srgb, yellow, transparent 50%); */
+    width: max-content;
+    margin: 0 auto;
+    background-color: var(--primary);
+    border-radius: 10px;
+    --x-deg: -5deg;
+    --y-deg: 20deg;
+    transform: perspective(1000px) rotateX(var(--x-deg)) rotateY(var(--y-deg));
+    transform-style: preserve-3d;
+
+    &::before {
+      content: "";
+      inset: 3px;
+      background-color: color-mix(in srgb, black, transparent 10%);
+      position: absolute;
+      --scale: 1;
+      transform: translateZ(-19px) scale(var(--scale));
+      filter: blur(5px);
+      border-radius: inherit;
+      z-index: -1;
+      opacity: .5;
+    }
+
+    &::after {
+      content: "";
+      inset: -10px;
+      background-color: color-mix(in srgb, yellow, transparent 50%);
+      position: absolute;
+      z-index: -2;
+      --scale: 1;
+      transform: translateZ(-20px) scale(var(--scale));
+      border-radius: inherit;
+    }
+  }
 
   .container {
-    padding: 100px 20px;
+    padding: 80px 50px;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    gap: 20px;
-    justify-items: center;
+    grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+    gap: 50px;
+    /* perspective: 1000px; */
 
     .lesson {
-      border: 4px solid color-mix(in srgb, black, transparent 80%);
+      /* border: 4px solid color-mix(in srgb, black, transparent 80%); */
       border-radius: 10px;
       transition: border-color 300ms, transform 300ms;
       position: relative;
+      --x-deg: -5deg;
+      --y-deg: 20deg;
+      transform: perspective(1000px) rotateX(var(--x-deg)) rotateY(var(--y-deg));
+      transform-style: preserve-3d;
 
       &:hover,
       &:focus {
-        border: 4px solid yellow;
-        transform: translateY(-20px);
+        /* border: 4px solid yellow; */
+        /* transform: translateY(-20px); */
+        /* transform: rotateY(20deg) rotateX(20deg); */
+        --x-deg: -5deg;
+        --y-deg: 0deg;
+        outline: none;
+
+        &::after {
+          /* transform: translateY(30px); */
+          /* --scale: 1.3; */
+          background-color: color-mix(in srgb, yellow, transparent 0%);
+        }
+        &::before {
+          /* --scale: 1.1; */
+          /* transform: translateZ(-30px) scale(1.1); */
+        }
+      }
+
+      &::before &::after {
+        transition: transform 300ms, background-color 300ms;
+      }
+
+      &::before {
+        content: "";
+        inset: 4px;
+        background-color: color-mix(in srgb, black, transparent 10%);
+        position: absolute;
+        --scale: 1;
+        transform: translateZ(-40px) scale(var(--scale));
+        filter: blur(4px);
+        border-radius: inherit;
+      }
+
+      &::after {
+        content: "";
+        inset: -15px;
+        background-color: color-mix(in srgb, yellow, transparent 50%);
+        position: absolute;
+        --scale: 1;
+        transform: translateZ(-40px) scale(var(--scale));
+        border-radius: inherit;
       }
 
       /* &::after {
         content: "";
-        width: 100%;
+        width: 90%;
+        position: absolute;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
         background-color: color-mix(in srgb, black, transparent 30%);
-        background-color: red;
-        height: 10px;
-        bottom: -20px;
+        background-color: red; */
+      /* height: 10px;
+        bottom: 0;
+        transition: transform 300ms;
+        filter: blur(5px);
+        opacity: .3;
+        z-index: -1;
       } */
 
       &__cover {
@@ -69,6 +162,7 @@ const lessons = [
 export default function LessonsSection() {
   return (
     <Section>
+      <h2 className="heading">LESSONS</h2>
       <div className="container">
         {lessons.map(lesson => (
           <NewTabLink
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,24 @@ import Seo from "../components/seo"
 import Landing from "../components/landing"
 import LessonsSection from "../components/lessons-section"
 import ReactConcepts from "../components/react-concepts"
+import styled from "styled-components"
+
+const FreeText = styled.div`
+  padding: 80px 0 100px;
+  font-size: 10rem;
+  color: white;
+  opacity: 0.4;
+  font-weight: 900;
+  letter-spacing: -2px;
+  transform: scaleY(0.8);
+`
 
 const IndexPage = () => (
   <Layout>
     <Landing />
+    <FreeText>
+      <span className="for-free-text">FOR FREE</span>
+    </FreeText>
     <LessonsSection />
     <ReactConcepts />
   </Layout>
